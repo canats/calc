01a6064d65d11aea6994ecba7742bb9c442f3754
Can <atescantr@gmail.com>
2022-07-03 17:43:44 +0300

    calc package created

---
+++ calc.go
@@ -0,0 +1,17 @@
+package calc
+
+func Total(a, b int) int {
+	return a + b
+}
+
+func Minus(a, b int) int {
+	return a - b
+}
+
+func Multi(a, b int) int {
+	return a * b
+}
+
+func Divide(a, b float64) float64 {
+	return a / b
+}
